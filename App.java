19fb82ab9db3d3964c1cc7c787e67f050bcc33aa
sanket <sanket_sakhardande@persistent.com>
2019-09-23 10:15:21 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,20 @@
+//test comment
+public class App{
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+        System.out.println("New commit");
+
+        System.out.println("heyy how you doin??");
+        git log --oneline
+        System.out.println("Master with updated data fixdefect pulling it");
+        
+        
+
+        System.out.println("Master with updated data hellooo");
+
+        System.out.println("Master with updated data fixdefect");
+
+
+
+    }
+}
